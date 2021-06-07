94567e25ce2badcb139e121b3f5ec9bb5803bb2f
DeriLone <58968194+DeriLone@users.noreply.github.com>
2021-06-08 00:20:15 +0300

    Add files via upload

---
+++ GameFactory/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using GameFactory.Factories;
+
+namespace GameFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMobsFactory[] lvls = new IMobsFactory[] {
+                new EzLvlFactory(),
+                new MidleLvlFactory(),
+                new HardLvlFactory()
+            };
+
+            Console.WriteLine("Выберете уровень сложности:");
+
+            for (int i = 0; i < lvls.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {lvls[i].ToString()}");
+            }
+
+            int lvlNumber = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            if (lvlNumber > -1 && lvlNumber < lvls.Length)
+            {
+                Game game = new Game(lvls[lvlNumber]);
+                game.StartGame();
+            }
+            else
+            {
+                Console.WriteLine("Недопустимое число");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
